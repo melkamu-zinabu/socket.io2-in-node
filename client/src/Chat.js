21161a3fd97c1154d1e0f6e93cfa9344766c08be
melkamu <zmelkamu918@gmail.com>
2023-04-30 16:37:06 +0300

    for chat component onkey enter functionality added

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css';
 // we need to the socket instance as props from app.js
 function Chat({socket,username,room}) {
     const [currentmessage, setcurrentmessage]=useState('')
+    //
+    const [messagelist, setmessagelist]=useState([])
+   
     const sendmessage=async()=>{
         if(currentmessage!==''){
             //all of the info that we send
@@ -13,17 +17,55 @@ function Chat({socket,username,room}) {
                 + new Date(Date.now()).getMinutes()
             }
             await socket.emit('sendmessage',messagedata)
+            setmessagelist((list)=>[...list,messagedata])
+          
         }
 
     }
+  //
+    useEffect(()=>{
+        socket.on('receivemessage',(data)=>{
+            setmessagelist((list)=>[...list,data])
+            console.log(data)
+             //you can see this meesage in web browesr by inspect except in the  one that triggered the server
+            // When the server sends a response using Socket.IO, 
+            // the response is sent back to all connected sockets 
+            // except for the one that triggered the server.
+                 })
+    },[socket])
   return (
-    <div>
-      <div className='chatheader'><p>live chat</p></div>
-      <div className='chatbody'></div>
-      <div className='chatfooter'>
+    <div className='chat-window'>
+      <div className='chat-header'><p>live chat</p></div>
+      <div className='chat-body'>
+        {messagelist.map((messagecontent)=>{
+            // When the server sends a response using Socket.IO, 
+            // the response is sent back to all connected sockets 
+            // except for the one that triggered the server.
+        // to have the message in my oun instance set the message list not only in socket.io also in socket.emit
+            return <div className='message' id={username===messagecontent.autor ? "you" : "other"}> 
+                <div>
+                    <div className='message-content'>
+                        <p>{messagecontent.message}</p>
+                    </div>
+                    <div className='message-meta'>
+                        <p>{messagecontent.time}</p>
+                        <p>{messagecontent.autor}</p>
+                    </div>
+               
+                </div>
+            </div>
+        })}
+      </div>
+      <div className='chat-footer'>
         <input type='text' placeholder='hey...'  onChange={(Event)=>{
        setcurrentmessage(Event.target.value)
-     }}/>
+     }}
+     // to send message when we press enter
+     
+     onKeyPress={(Event)=>{
+        Event.key==="Enter" && sendmessage()
+     }}
+     />
         <button onClick={sendmessage}>&#9658;</button>
       </div>
     </div>
@@ -31,3 +73,17 @@ function Chat({socket,username,room}) {
 }
 
 export default Chat
+
+
+// // Client side
+// socket.emit('my_event', 'Hello, server!', (response) => {
+//     console.log(response);
+//   });
+  
+//   // Server side
+//   socket.on('my_event', (data, callback) => {
+//     console.log(data);
+//     // send response back to the specific client
+//     callback('Hello, client!');
+//   });
+  
